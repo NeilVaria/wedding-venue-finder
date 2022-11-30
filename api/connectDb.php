a72fa16bcda023fc0b81b4153c8263c6f5f82c70
Neil Varia <neilvaria@gmail.com>
2022-11-30 05:37:07 +0000

    removed redundant files

--- api/connectDb.php
+++
@@ -1,9 +0,0 @@
-<?php
-$servername = "rvis.ddns.net:13306";
-$username = "nvaria";
-$password = "Belmont47";
-$dbname = "coa123wdb";
-$conn = new mysqli($servername, $username, $password, $dbname);
-
-echo get_include_path();
-var_dump(stream_resolve_include_path("db.php"));
